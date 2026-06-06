af4e11a4999d8a97452d3b10280a3c97ffe693d4
abdulrehmangull495 <abdulrehmangull495@gmail.com>
2026-06-06 14:47:01 +0500

    update code

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -330,8 +330,7 @@ const Shop = () => {
         <div className="subsdiv">
           <h6 className="subscribeto">Subscribe To Our Newsletter</h6>
           <p className="getthe">
-            Get the best deals and product updates delivered straight to your
-            inbox.
+           Be the first to know about new products, offers and discounts.
           </p>
           <p>abdulrehmangull495@gmail.com</p>
           <div>
