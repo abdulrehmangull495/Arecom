0ebd7814fab9b834863f474ba88c300b8a3c6683
abdulrehmangull495 <abdulrehmangull495@gmail.com>
2026-06-11 16:55:56 +0500

    update code

--- src/components/Forget.jsx
+++ src/components/Forget.jsx
@@ -54,7 +54,7 @@ const Forget = () => {
 
       </div>
     </div>
-    {/* <Footer/> */}
+   
    </>
   )
 }
--- src/components/Mycheckout.jsx
+++ src/components/Mycheckout.jsx
@@ -100,28 +100,8 @@ const Mycheckout = () => {
       </div>
 
       
-      {/* <footer className="footer">
-        <div className="d-flex ftdiv">
-  <div class="footer-box">
-    
-    <p className="m-0 arecom">Arecom</p>
-  </div>
+     
 
-  <div className="footer-box">
-  
-
-    <a href="#">Support Center</a>
-    <a href="/Checkout">Invoicing</a>
-    
-    <a href="#">Blog</a>
-  </div>
-  </div>
-  <div className="copydiv">
-    <p className="copy">Copyright © 2026 arecom. All Rights Reseved.</p>
-  </div>
-</footer> */}
-
-{/* <Footer/> */}
     </>
   )
 }
