656b5519b8eced9f2d8d55871ced4ee90b2aa621
abdulrehmangull495 <abdulrehmangull495@gmail.com>
2026-07-28 17:41:49 +0500

    add framer motion effects in home.jsx page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,7 @@ import Footer from "./Footer";
 import ScrollToTop from "./ScrollToTop";
 import ProductCard from "./ProductCard";
 import { motion } from "framer-motion";
+const MotionNavLink = motion(NavLink);
 
 const products = [
   {
@@ -199,32 +200,58 @@ const Home = () => {
 
           {/* initial={{ opacity: 0, y: -50 }}
                 animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.7 }} */}
+                transition={{ duration: 0.7 }} 
+                */}
           {/* <img src="louislogo.png" alt="louislogo" />
           <img src="pradalogo.png" alt="pradalogo" />
           <img src="denim.png" alt="denim" />
           <img src="calvinklein.png" alt="calvinklein" /> */}
         </motion.div>
 
-        <div className="lastdatesection">
-          <div className="dealssection">
-            <h3 className="dealsof">Deals Of The Month</h3>
-            <p className="exc">
-              Don't miss this month's exclusive offers. Shop now and enjoy
-              special prices for a limited time.
-            </p>
-            <NavLink to="/shop" className="btnshopnow">
-              Shop Now
-            </NavLink>
-            <h3 className="hurry">Hurry, Before It's Too Late!</h3>
-          </div>
-          <div className="mixgirlimg">
+        <motion.div className="lastdatesection">
+          <motion.div
+            //  initial={{opacity:0, x:-200}}
+            //   animate={{opacity:1, x:0}}
+            //   transition={{duration:0.8,
+            //   delay: 0.2}}
+            initial={{ opacity: 0, x: -200 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8 }}
+            viewport={{ once: true, amount: 0.3 }}
+          >
+            <div className="dealssection">
+              <h3 className="dealsof">Deals Of The Month</h3>
+              <p className="exc">
+                Don't miss this month's exclusive offers. Shop now and enjoy
+                special prices for a limited time.
+              </p>
+
+              <div>
+                <NavLink to="/shop" className="btnshopnow">
+                  Shop Now
+                </NavLink>
+              </div>
+              <h3 className="hurry">Hurry, Before It's Too Late!</h3>
+            </div>
+          </motion.div>
+          <motion.div
+            className="mixgirlimg"
+            initial={{ opacity: 0, x: 300 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
+            viewport={{ once: true, amount: 0.3 }}
+          >
             <img className="blackgirlimg" src="blackgirl.png" alt="blackgirl" />
             <img className="bluegirlimg" src="bluegirl.png" alt="bluegirl" />
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
 
-        <div className="newarrivalsection">
+        <motion.div className="newarrivalsection"
+        initial={{ opacity: 0, x: 300 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
+            viewport={{ once: true, amount: 0.3 }}
+        >
           <div className="newarrivals">
             <h1 className="newarrival">New Arrivals</h1>
             <p className="justdropped">
@@ -267,9 +294,15 @@ const Home = () => {
               View More
             </Button>
           </div>
-        </div>
+        </motion.div>
 
-        <div className="peakysectionhome">
+        <motion.div
+          className="peakysectionhome"
+          initial={{ opacity: 0, x: 300 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+          viewport={{ once: true, amount: 0.3 }}
+        >
           <div>
             <img className="girlside" src="girlsidepose.png" alt="girlside" />
           </div>
@@ -290,8 +323,14 @@ const Home = () => {
               Shop Now
             </NavLink>
           </div>
-        </div>
-        <div className="fouritemsdiv">
+        </motion.div>
+        <motion.div
+          className="fouritemsdiv"
+          initial={{ opacity: 0, y: 200 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+          viewport={{ once: true, amount: 0.4 }}
+        >
           <div className="qualitydiv">
             <div>
               <img className="icon" src="support.png" alt="support" />
@@ -329,8 +368,14 @@ const Home = () => {
               <p className="mb-0 dedicated">Dedicated support</p>
             </div>
           </div>
-        </div>
-        <div className="lastsection">
+        </motion.div>
+        <motion.div
+          className="lastsection"
+          initial={{ opacity: 0, y: 200 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+          viewport={{ once: true, amount: 0.6 }}
+        >
           <div>
             <img src="camelcoat.png" className="camelcoatpic" alt="camelcoat" />
           </div>
@@ -355,7 +400,7 @@ const Home = () => {
               alt="darkgreycoat"
             />
           </div>
-        </div>
+        </motion.div>
         <ScrollToTop />
       </main>
       <Footer />
